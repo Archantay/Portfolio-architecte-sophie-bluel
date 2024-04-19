b85ad5059e846d0689e5c110af63c0d35b7cbd81
lutri <lutringerkevin@gmail.com>
2024-04-19 02:44:26 +0200

    changement quand connecté

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,94 +1,119 @@
 const portfolio = document.getElementById('portfolio');
+let menuCategories;
 
 document.addEventListener('DOMContentLoaded', () => {
-  afficherGalerie('Tous');
+    fetchCategories();
+    afficherGalerie('Tous');
+    checkUserAuthentication();
 });
+
 function afficherGalerie(categorieSelectionnee) {
-
-  let gallery = portfolio.querySelector('.gallery');
-  
-  fetch('http://localhost:5678/api/works')
-    .then(response => response.json()) 
-    .then(data => {
-    
-      if (!gallery) {
-        gallery = document.createElement('div');
-        gallery.classList.add('gallery');
-        portfolio.appendChild(gallery);
-      } else {
-        gallery.innerHTML = ''; 
-      }
-
-      data.forEach(projets => {
-        if (categorieSelectionnee === 'Tous' || projets.category.name === categorieSelectionnee) {
-          const figure = document.createElement('figure');
-          const img = document.createElement('img');
-          img.src = projets.imageUrl;
-          img.alt = projets.title;
-          figure.appendChild(img);
-          const figcaption = document.createElement('figcaption');
-          figcaption.textContent = projets.title;
-          figure.appendChild(figcaption);
-          gallery.appendChild(figure);
-        }
-      });
-    })
-    .catch(error => {
-      console.error('Une erreur s\'est produite lors de la récupération des données :', error);
-    });
-}
-function fetchCategories() {
-  fetch('http://localhost:5678/api/categories')
-    .then(response => response.json()) 
-    .then(categories => {
-      const menuCategories = document.createElement('div');
-      menuCategories.classList.add('categories');
-      const portfolio = document.getElementById('portfolio');
-      portfolio.appendChild(menuCategories);
-      
-      const btnTous = document.createElement('button');
-      btnTous.textContent = 'Tous';
-      btnTous.addEventListener('click', () => {
-        afficherGalerie('Tous');
-        console.log('Afficher tous les projets');
-      });
-      menuCategories.appendChild(btnTous);
-      
-      categories.forEach(category => {
-        const btnCategorie = document.createElement('button');
-        btnCategorie.textContent = category.name;
-        btnCategorie.addEventListener('click', () => {
-          afficherGalerie(category.name);
-          console.log('Afficher les projets de la catégorie :', category.name);
+    let gallery = portfolio.querySelector('.gallery');
+    fetch('http://localhost:5678/api/works')
+        .then(response => response.json())
+        .then(data => {
+            if (!gallery) {
+                gallery = document.createElement('div');
+                gallery.classList.add('gallery');
+                portfolio.appendChild(gallery);
+            } else {
+                gallery.innerHTML = '';
+            }
+            data.forEach(projet => {
+                if (categorieSelectionnee === 'Tous' || projet.category.name === categorieSelectionnee) {
+                    const figure = document.createElement('figure');
+                    const img = document.createElement('img');
+                    img.src = projet.imageUrl;
+                    img.alt = projet.title;
+                    figure.appendChild(img);
+                    const figcaption = document.createElement('figcaption');
+                    figcaption.textContent = projet.title;
+                    figure.appendChild(figcaption);
+                    gallery.appendChild(figure);
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la récupération des données :', error);
         });
-        menuCategories.appendChild(btnCategorie);
-      });
-    })
-    .catch(error => {
-      console.error('Une erreur s\'est produite lors de la récupération des catégories :', error )
-    });
-};
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-  const token = localStorage.getItem('token');
-  if (token) {
-      const menuCategories = document.querySelector('.categories');
-      if (menuCategories) {
-          menuCategories.style.display = 'none';
-      }
-  } else {
-      fetchCategories();
-  }
+function fetchCategories() {
+    fetch('http://localhost:5678/api/categories')
+        .then(response => response.json())
+        .then(categories => {
+            const menuCategories = document.createElement('div');
+            menuCategories.classList.add('categories');
+            const portfolio = document.getElementById('portfolio');
+
+            const token = localStorage.getItem('token');
+            if (token) {
+                menuCategories.style.display = 'none';
+              
+            }
+            portfolio.appendChild(menuCategories);
+
+            const btnModifier = document.createElement('button');
+            btnModifier.textContent = 'modifier';
+            const iconModifier = document.createElement('i');
+            iconModifier.classList.add('fa-regular', 'fa-pen-to-square');
+            btnModifier.insertBefore(iconModifier, btnModifier.firstChild);
+            btnModifier.addEventListener('click', () => {
+                // Ajouter ici la logique pour la modification
+                console.log('Bouton Modifier cliqué');
+            });
+            if (!token) {
+              btnModifier.style.display = 'none';
+              menuCategories.style.display = 'visible';
+            
+            }
+            titreGallery.appendChild(btnModifier);
+          
+
+            const btnTous = document.createElement('button');
+            btnTous.textContent = 'Tous';
+            btnTous.addEventListener('click', () => {
+                afficherGalerie('Tous');
+                console.log('Afficher tous les projets');
+            });
+            menuCategories.appendChild(btnTous);
+
+            categories.forEach(category => {
+                const btnCategorie = document.createElement('button');
+                btnCategorie.textContent = category.name;
+                btnCategorie.addEventListener('click', () => {
+                    afficherGalerie(category.name);
+                    console.log('Afficher les projets de la catégorie :', category.name);
+                });
+                menuCategories.appendChild(btnCategorie);
+            });
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la récupération des catégories :', error)
+        });
+}
+
+function checkUserAuthentication() {
+    const token = localStorage.getItem('token');
+    const loginLink = document.getElementById('loginLink');
+
+    if (token) {
       
-  const loginLink = document.getElementById('loginLink');
-  if (loginLink) {
-      loginLink.textContent = 'Logout';
-     
-      loginLink.addEventListener('click', () => {
-          localStorage.removeItem('token');
-          window.location.href = 'index.html';
-      });
-  }
-  
-
-});
+        if (loginLink) {
+            loginLink.textContent = 'Logout';
+            loginLink.addEventListener('click', () => {
+                localStorage.removeItem('token');
+                location.reload();
+            });
+        }
+    } else {
+      
+        if (loginLink) {
+            loginLink.textContent = 'Login';
+            loginLink.addEventListener('click', () => {
+                window.location.href = 'login.html';
+            });
+           
+        }
+    }
+}
