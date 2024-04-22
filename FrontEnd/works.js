b0baad35aeffd95c6e33f13d6b56692f7e54cd5d
lutri <lutringerkevin@gmail.com>
2024-04-22 13:25:51 +0200

    correction code

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,13 +1,13 @@
 const portfolio = document.getElementById('portfolio');
-let menuCategories;
+let miniaturesAppelees = false;
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchCategories();
-    afficherGalerie('Tous');
+    afficherGalerie();
     checkUserAuthentication();
 });
 
-function afficherGalerie(categorieSelectionnee) {
+function afficherGalerie(categorieSelectionnee = 'Tous') {
     let gallery = portfolio.querySelector('.gallery');
     fetch('http://localhost:5678/api/works')
         .then(response => response.json())
@@ -34,11 +34,13 @@ function afficherGalerie(categorieSelectionnee) {
             });
         })
         .catch(error => {
-            console.error('Une erreur s\'est produite lors de la récupération des données :', error);
+            alert('Une erreur est survenue')
         });
 }
 
 function fetchCategories() {
+    let menuCategories;
+
     fetch('http://localhost:5678/api/categories')
         .then(response => response.json())
         .then(categories => {
@@ -49,7 +51,6 @@ function fetchCategories() {
             const token = localStorage.getItem('token');
             if (token) {
                 menuCategories.style.display = 'none';
-              
             }
             portfolio.appendChild(menuCategories);
 
@@ -62,28 +63,27 @@ function fetchCategories() {
                 var modal = document.getElementById("myModal");
                 var span = document.getElementsByClassName("close")[0];
                 modal.style.display = "block";
-                span.onclick = function() {
+                span.onclick = function () {
                     modal.style.display = "none";
                 }
-                window.onclick = function(event) {
+                window.onclick = function (event) {
                     if (event.target == modal) {
                         modal.style.display = "none";
                     }
                 }
+                afficherMiniatures()
+
             });
             if (!token) {
-              btnModifier.style.display = 'none';
-              menuCategories.style.display = 'visible';
-            
+                btnModifier.style.display = 'none';
+                menuCategories.style.display = 'visible';
             }
             titreGallery.appendChild(btnModifier);
-          
 
             const btnTous = document.createElement('button');
             btnTous.textContent = 'Tous';
             btnTous.addEventListener('click', () => {
-                afficherGalerie('Tous');
-                console.log('Afficher tous les projets');
+                afficherGalerie();
             });
             menuCategories.appendChild(btnTous);
 
@@ -92,13 +92,12 @@ function fetchCategories() {
                 btnCategorie.textContent = category.name;
                 btnCategorie.addEventListener('click', () => {
                     afficherGalerie(category.name);
-                    console.log('Afficher les projets de la catégorie :', category.name);
                 });
                 menuCategories.appendChild(btnCategorie);
             });
         })
-        .catch(error => {
-            console.error('Une erreur s\'est produite lors de la récupération des catégories :', error)
+        .catch(() => {
+            alert('Une erreur est survenue')
         });
 }
 
@@ -106,23 +105,53 @@ function checkUserAuthentication() {
     const token = localStorage.getItem('token');
     const loginLink = document.getElementById('loginLink');
 
-    if (token) {
-      
-        if (loginLink) {
+    if (loginLink) {
+        if (token) {
             loginLink.textContent = 'Logout';
             loginLink.addEventListener('click', () => {
                 localStorage.removeItem('token');
                 location.reload();
             });
-        }
-    } else {
-      
-        if (loginLink) {
+        } else {
             loginLink.textContent = 'Login';
             loginLink.addEventListener('click', () => {
                 window.location.href = 'login.html';
             });
-           
         }
     }
 }
+
+function afficherMiniatures() {
+    const thumbnailsContainer = document.querySelector('.thumbnails');
+
+    if (!miniaturesAppelees) {
+        fetch('http://localhost:5678/api/works')
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(projet => {
+                    const miniature = document.createElement('img');
+                    miniature.src = projet.imageUrl;
+                    miniature.alt = projet.title;
+                    miniature.classList.add('thumbnail');
+
+                    const deleteIcon = document.createElement('i');
+                    deleteIcon.classList.add('fas', 'fa-trash-alt', 'fa-xs', 'delete-icon');
+                    deleteIcon.addEventListener('click', () => {
+                        // Action à effectuer pour supprimer un projet
+                    });
+
+                    const thumbnailContainer = document.createElement('div');
+                    thumbnailContainer.classList.add('thumbnail-container');
+                    thumbnailContainer.appendChild(miniature);
+                    thumbnailContainer.appendChild(deleteIcon);
+
+                    thumbnailsContainer.appendChild(thumbnailContainer);
+                });
+
+                miniaturesAppelees = true;
+            })
+            .catch(() => {
+                alert('Une erreur est survenue')
+            });
+    }
+};
